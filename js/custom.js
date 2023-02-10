ec08036ec34283408c18f65158cb408c2e3c7b92
prabath23 <prabath@archmage.lk>
2023-02-10 17:09:31 +0530

    html

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+
+  // ------------scroll navbar------------
+
+  $(document).ready(function(){
+    var scrollTop = 0;
+    $(window).scroll(function(){
+      scrollTop = $(window).scrollTop();
+
+      if (scrollTop >= 100) {
+        $('.navbar').addClass('scrolled-nav');
+
+      } else if (scrollTop < 100) {
+        $('.navbar').removeClass('scrolled-nav');
+      }
+    });
+  });
+
